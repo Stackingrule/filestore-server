638a4524e6d3ad273dabcf7a7d204710d073ca45
Stackingrule <38368554+Stackingrule@users.noreply.github.com>
2021-01-17 09:52:16 +0800

    1.用户注册接口实现

--- main.go
+++ main.go
@@ -12,9 +12,12 @@ func main() {
 	http.HandleFunc("/file/upload", handler.UploadHandler)
 	http.HandleFunc("/file/upload/suc", handler.UploadSucHandler)
 	http.HandleFunc("/file/meta", handler.GetFileMetaHandler)
+
 	http.HandleFunc("/file/download", handler.DownloadHandler)
 	http.HandleFunc("/file/update", handler.UpdateHandler)
 	http.HandleFunc("/file/delete", handler.FileDeleteHandler)
+
+	http.HandleFunc("/user/signup", handler.SignupHandler)
 
 	err := http.ListenAndServe(":8089", nil) //设置监听的端口
 	if err != nil {
